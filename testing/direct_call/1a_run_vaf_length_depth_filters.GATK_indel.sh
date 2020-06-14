d6b6bef8e1dddf94903573e977aa73bcc979d079
Matthew A. Wyczalkowski <m.wyczalkowski@wustl.edu>
2020-06-14 18:12:34 +0000

    Successful testing of sample rename in vcf.gz files

--- testing/direct_call/1a_run_vaf_length_depth_filters.GATK_indel.sh
+++ testing/direct_call/1a_run_vaf_length_depth_filters.GATK_indel.sh
@@ -2,11 +2,12 @@
 OUTD="/data/VLD_FilterVCF.out"
 mkdir -p $OUTD
 
-VCF="/data/GATK.indel.Final.vcf"
+VCF="/data/GATK.indel.Final.vcf.gz"
 #VCF="/data/GATK.indel.Final-short.vcf"
 CONFIG="../../params/VLD_FilterVCF-GATK.config.ini"
 
-OUT="$OUTD/GATK.indel.VLD.vcf"
+OUT="$OUTD/GATK.indel.VLD.SAMPLE.vcf"
+ARG="-s SAMPLE"
 
-bash ../../src/run_vaf_length_depth_filters.sh $@ -o $OUT $VCF $CONFIG
+bash ../../src/run_vaf_length_depth_filters.sh $@ $ARG -o $OUT $VCF $CONFIG
 
